5c79ab89f2c380071cadea7ae9dd1e6bdfa2593d
Jarno Luoma-Nirva <jln@iki.fi>
2016-09-18 11:58:17 +0300

    Projects page lists projects.

--- tasks/tests/test_views_project.py
+++ tasks/tests/test_views_project.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from tasks.models import Project
 
 
 class ProjectListTest(TestCase):
@@ -10,7 +12,15 @@ class ProjectListTest(TestCase):
         response = self.client.get(reverse('tasks:projects'))
         self.assertTemplateUsed(response, 'tasks/project_list.html')
 
-    def test_default_content(self):
+    def test_default_context(self):
         response = self.client.get(reverse('tasks:projects'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Projects')
+        self.assertEqual(response.context['project_list'].count(), 0)
+        creator = User.objects.create(username='creator')
+        project = Project.objects.create(name='test_project', title='Test project', created_by=creator)
+        response = self.client.get(reverse('tasks:projects'))
+        self.assertEqual(response.context['project_list'].count(), 1)
+        self.assertEqual(response.context['project_list'][0], project)
+        self.assertContains(response, project.title)
+        Project.objects.create(name='test_project_2', created_by=creator)
+        response = self.client.get(reverse('tasks:projects'))
+        self.assertEqual(response.context['project_list'].count(), 2)
