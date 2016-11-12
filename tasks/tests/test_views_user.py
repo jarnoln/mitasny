e91e92ac7d6e77c1eb965c4ebf81cf5088d17731
Jarno Luoma-Nirva <jln@iki.fi>
2016-11-12 14:28:44 +0200

    Refactor:User tests to own file

---
+++ tasks/tests/test_views_user.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib import auth
+
+
+class UserTest(TestCase):
+    def test_login_get(self):
+        response = self.client.get(reverse('login'))
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_login_post(self):
+        user = auth.models.User.objects.create(username='creator', email='creator@iki.fi')
+        user.set_password('pw')
+        user.save()
+        response = self.client.post(reverse('login'), {'username': user.username, 'password': 'pw', 'next': reverse('tasks:home')}, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated())
+        self.assertEqual(response.context['user'], user)
+        self.assertTemplateUsed(response, 'tasks/project_list.html')
+
+    def test_logout(self):
+        user = auth.models.User.objects.create(username='creator', email='creator@iki.fi')
+        user.set_password('pw')
+        user.save()
+        response = self.client.post(reverse('login'), {'username': user.username, 'password': 'pw', 'next': reverse('tasks:home')}, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated())
+        response = self.client.get(reverse('logout'))
+        self.assertFalse(response.context['user'].is_authenticated())
+        self.assertTemplateUsed(response, 'registration/logged_out.html')
+
+    def test_logout_redirect(self):
+        user = auth.models.User.objects.create(username='creator', email='creator@iki.fi')
+        user.set_password('pw')
+        user.save()
+        response = self.client.post(reverse('login'), {'username': user.username, 'password': 'pw', 'next': reverse('tasks:home')}, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated())
+        response = self.client.get(reverse('logout') + '?next=' + reverse('tasks:projects'), follow=True)
+        self.assertFalse(response.context['user'].is_authenticated())
+        self.assertTemplateUsed(response, 'tasks/project_list.html')
+
+    def test_register_view(self):
+        response = self.client.get(reverse('tasks:register'))
+        self.assertTemplateUsed(response, 'registration/register.html')
+
+    def test_register_user(self):
+        self.assertEqual(auth.models.User.objects.count(), 0)
+        response = self.client.post(reverse('tasks:register'), data={
+            'username': 'testuser',
+            'password1': 'pass',
+            'password2': 'pass'}, follow=True)
+        self.assertEqual(auth.models.User.objects.count(), 1)
+        user = auth.models.User.objects.all()[0]
+        self.assertEqual(user.username, 'testuser')
+        self.assertTemplateUsed(response, 'tasks/project_list.html')
+        self.assertTrue(response.context['user'].is_authenticated())
+        self.assertEqual(response.context['user'], user)
